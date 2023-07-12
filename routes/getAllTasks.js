34e2d234c27291230dc347f5e64f2ecbee9124bf
Pranav T V <PranavPadmanabhan77@gmail.com>
2023-07-12 13:35:29 +0530

    Update getAllTasks.js

--- routes/getAllTasks.js
+++ routes/getAllTasks.js
@@ -4,7 +4,7 @@ const router = require("express").Router()
 
 
 router.get("/",async(req,res) => {
-    Tasks.collection.find({}).toArray((err, data) => {
+    await Tasks.find({}).then((data) => {
         res.status(200).json(data);
       });
 })
